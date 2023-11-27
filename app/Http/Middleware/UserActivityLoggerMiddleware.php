cbeded3808ea1c7a3f11d7dd911b632695ed680e
chuprasov <125488152+chuprasov@users.noreply.github.com>
2023-11-27 16:06:28 +0300

    log user actions

---
+++ app/Http/Middleware/UserActivityLoggerMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Log;
+
+class UserActivityLoggerMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user_id = $request->user()->id;
+        $action = $request->route()->getName();
+        $ip = $request->ip();
+
+        Log::info('User action: ', [
+            'user_id' => $user_id,
+            'action' => $action,
+            'ip' => $ip,
+        ]);
+
+        return $next($request);
+    }
+}
