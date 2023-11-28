54e52bf96204b8844f556ade0c338c33c34077b5
chuprasov <125488152+chuprasov@users.noreply.github.com>
2023-11-28 12:47:46 +0300

    proxy *

--- app/Http/Middleware/UserActivityLoggerMiddleware.php
+++ app/Http/Middleware/UserActivityLoggerMiddleware.php
@@ -19,16 +19,6 @@ class UserActivityLoggerMiddleware
         $user_id = $request->user()->id;
         $action = $request->route()->getName();
         $ip = $request->getClientIps();
-        // foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key){
-        //     if (array_key_exists($key, $_SERVER) === true){
-        //         foreach (explode(',', $_SERVER[$key]) as $ip){
-        //             $ip = trim($ip); // just to be safe
-        //             if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false){
-        //                 return $ip;
-        //             }
-        //         }
-        //     }
-        // }
 
         Log::info('User action: ', [
             'user_id' => $user_id,
